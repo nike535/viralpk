b7470cbefa1ca42529e2596dbc983ccdd2811ed3
akhilomar <36676369+akhilomar@users.noreply.github.com>
2023-04-06 00:10:22 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -94,7 +94,7 @@ const Post: React.FC<PostProps> = (props) => {
 				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
 				<meta
 					property="og:image:alt"
-					content={post.featuredImage.node.altText || post.title}
+					content={"https://www.viralpostking.com/lord-shiva-born-story-in-hindi/"}
 				/>
 				<title>{post.title}</title>
 			</Head>
